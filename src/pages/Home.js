c5039739d1a0c7b12c1e8c4eb8b79b2c31128e2c
hoodboy <littlepark0107@gmail.com>
2021-10-10 22:36:13 +0900

    feat: add Subscribe block

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,13 +6,28 @@ import Header from '../components/Header';
 const Home = () => {
     return(
         <PageBlock>
-        <Header></Header>
-        <Space></Space>
-        <MainStatue>
+            <Header/>
+            <MainStatue>
 
-        </MainStatue>
-        <Space></Space>
+            </MainStatue>
+            <SubscribeBlock>
+                <SubscribeTitle>
+                Subscribe to our newsletter
 
+                </SubscribeTitle>
+                <SubscribeSub>
+                New Products, Sales, & Promos. Directly to your inbox.
+
+
+                </SubscribeSub>
+                <EmailWrapper>
+                    <EmailInput placeholder="E-mail" />
+                    <SubscribeButton>Subscribe</SubscribeButton>
+                </EmailWrapper>
+            </SubscribeBlock>
+            <Footer>
+            © 2021, shroom
+            </Footer>
         </PageBlock>
     )    
 }
@@ -21,18 +36,13 @@ const Home = () => {
 const PageBlock = styled.div`
     width: 100%;
     max-width: 1000px;
-    margin: 8rem auto;
+    margin: 0 auto;
 `
-
-const Space = styled.div`
-    width: 100%;
-    height: 100vh;
-`;
 
 const MainStatue = styled.div`
     width: 100rem;
     height: 100rem;
-    margin: 0 auto;
+    margin: 50rem auto;
     background-image: url(${Mdma});
     background-size: contain;
     background-repeat: no-repeat;
@@ -48,4 +58,46 @@ const MainStatue = styled.div`
         }
 `;
 
+const SubscribeBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const SubscribeTitle = styled.div`
+    font-size: 8rem;
+    margin-bottom: 2rem;
+`;
+
+const SubscribeSub = styled.div`
+    font-size: 3rem;
+    margin-bottom: 30rem;
+`;
+
+const EmailWrapper = styled.div`
+    display: flex;
+`;
+
+const EmailInput = styled.input`
+    padding: 3rem 5rem;
+    outline: none;
+    border: none;
+    width: 300px;
+`;
+
+const SubscribeButton = styled.div`
+    padding: 3rem 5rem;
+    font-size: 3rem;
+    color: #fff;
+    background: #1f2021;
+`;
+
+const Footer = styled.div`
+    width: 100%;
+    justify-content: center;
+    text-align: center;
+    margin: 60rem auto 10rem auto;
+    font-size: 3rem;
+`;
 export default Home;
