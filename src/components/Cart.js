0f0e249256163ab25945441f0eb594c64a7def65
hoodboy <littlepark0107@gmail.com>
2021-10-16 22:09:42 +0900

    feat: create cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,7 @@
 import React, {useState} from 'react';
 import styled, {css, keyframes} from 'styled-components';
-import Cancle from '../assets/user2.png';import { Link } from "react-router-dom";
-import LogoImg from '../assets/mushroom.png';
-import Profileimg from '../assets/user.png';
-import CartImg from '../assets/shopping-cart.png';
-
+import Cancle from '../assets/user2.png';
+import {Link} from 'react-router-dom';
 
 const Wrapper = styled.div`
     display: flex;
@@ -104,43 +101,14 @@ const MenuContent = styled.div`
     width: 150px;
 `;
 
-
-const Header = () => {
-
+const Cart = () => {
 
     const [open, setOpen] = useState(false);
     const onToggle = () => {
         setOpen(!open)
     }
 
-    return(
-        <HeaderBlock>
-            <Link to="/" >
-                <Logo/>
-            </Link>
-            <NavWrapper>
-                <NavBar>
-                    <Link to="/shop" style={{ textDecoration: "none", color: "#000" }}>
-                        <Shop>Shop</Shop>
-                    </Link>
-                    <LookBook>
-                        Lookbook
-                    </LookBook>
-                    <StockList>
-                        Stocklist
-                    </StockList>
-                    <Trip>
-                    <a href="http://iacopoapps.appspot.com/hopalongwebgl/" style={{ textDecoration: "none", color: "#000" }}>
-
-                        Trip
-                        </a>
-                    </Trip>
-                </NavBar>
-                <Link to="/login" style={{ textDecoration: "none", color: "#000" }}>
-                    <Profile/>
-                </Link>
-                <Cart onClick={onToggle} />
-            </NavWrapper>
+    return (
             <Wrapper open={open}>
                 <ModalWrapper>
                     <CancleButton onClick={onToggle}/>
@@ -166,90 +134,7 @@ const Header = () => {
                 </ModalWrapper>
                 <ModalOpacity onClick={onToggle}/>
             </Wrapper>
-        </HeaderBlock>
-    )    
+    )
 }
 
-const HeaderBlock = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    position: sticky;
-    top: 8rem;
-    z-index: 1;
-`
-
-const Logo = styled.div`
-    width: 10rem;
-  height: 10rem;
-    border-radius: 5rem;
-  background-image: url(${LogoImg});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  transform: scale(1);
-  transition: all 0.4s ease-in-out;
-  &:hover {
-    transform: scale(1.07);
-  }
-  `
-
-const NavWrapper = styled.div`
-    display: flex;
-    align-items: center;
-`
-
-const NavBar = styled.div`
-    display: flex;
-`
-
-const Shop = styled.div`
-    padding: 0 5rem;
-    &:hover{
-        color: #3D3D3D;
-        cursor: pointer;
-
-    }
-`
-
-const LookBook = styled(Shop)`
-
-`
-
-const StockList = styled(Shop)`
-
-`
-const Trip = styled(Shop)`
-
-`
-
-const Profile = styled.div`
-    width: 5rem;
-    height: 5rem;
-    margin: 0 5rem;
-    background-image: url(${Profileimg});
-background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  transform: scale(1);
-  transition: all 0.4s ease-in-out;
-  &:hover {
-    transform: scale(1.07);
-  }
-`
-
-const Cart = styled(Profile)`
-margin: 0;
-    background-image: url(${CartImg});
-
-`
-
-export default Header;
+export default Cart;
