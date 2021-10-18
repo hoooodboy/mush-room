cc2927e81edce7aecca129ed50efcdbf8a571551
hoodboy <littlepark0107@gmail.com>
2021-10-18 22:00:10 +0900

    chore: add menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,167 @@ import LogoImg from '../assets/mushroom.png';
 import Profileimg from '../assets/user.png';
 import CartImg from '../assets/shopping-cart.png';
 import EmptyCartImg from '../assets/emptycart.png'
+import MenuImg from '../assets/menu.png'
+
+const Header = () => {
+
+    const [open, setOpen] = useState(false);
+    const [close, setClose] = useState(true);
+
+    const onToggle = () => {
+        setOpen(!open)
+        setClose(!close)
+    }
+    console.log(close)
+
+    return(
+        <>
+        <HeaderBlock>
+            <MenuIcon/>
+            <Link to="/" >
+                <Logo/>
+            </Link>
+            <NavWrapper>
+                <NavBar>
+                    <Link to="/shop" style={{ textDecoration: "none", color: "#000" }}>
+                        <Shop>Shop</Shop>
+                    </Link>
+                    <LookBook>
+                        Lookbook
+                    </LookBook>
+                    <StockList>
+                        Stocklist
+                    </StockList>
+                    <Trip>
+                    <a href="http://iacopoapps.appspot.com/hopalongwebgl/" style={{ textDecoration: "none", color: "#000" }}>
+                        Trip
+                        </a>
+                    </Trip>
+                </NavBar>
+                <Link to="/login" style={{ textDecoration: "none", color: "#000" }}>
+                    <Profile/>
+                </Link>
+                <Cart onClick={onToggle} />
+            </NavWrapper>
+        </HeaderBlock>
+        <Wrapper open={open}>
+            <CartWrapper close={close}>
+                <CancleWrapper>
+                    <CancleTitle>Cart</CancleTitle>
+                    <CancleButton onClick={onToggle}/>
+                </CancleWrapper>
+                <EmptyCart/>
+                Cart is empty
+            </CartWrapper>
+            <CartOpacity onClick={onToggle}/>
+        </Wrapper>
+    </>
+    )    
+}
+
+const HeaderBlock = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: sticky;
+    top: 8rem;
+    z-index: 1;
+    @media screen and (max-width: 1024px) {
+        padding: 0 5%;
+  }
+`
+
+const Logo = styled.div`
+    width: 10rem;
+  height: 10rem;
+    border-radius: 5rem;
+  background-image: url(${LogoImg});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  transform: scale(1);
+  transition: all 0.4s ease-in-out;
+  &:hover {
+    transform: scale(1.07);
+  }
+  `
+
+const NavWrapper = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const NavBar = styled.div`
+    display: flex;
+    @media screen and (max-width: 1024px) {
+    display: none;
+  }
+`
+
+const Shop = styled.div`
+    padding: 0 5rem;
+    &:hover{
+        color: #3D3D3D;
+        cursor: pointer;
+    }
+`
+
+const LookBook = styled(Shop)`
+
+`
+
+const StockList = styled(Shop)`
+
+`
+const Trip = styled(Shop)`
+
+`
+
+const Profile = styled.div`
+    width: 5rem;
+    height: 5rem;
+    margin: 0 5rem;
+    background-image: url(${Profileimg});
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    transform: scale(1);
+    transition: all 0.4s ease-in-out;
+    &:hover {
+        transform: scale(1.07);
+  }
+  @media screen and (max-width: 1024px) {
+    display: none;
+  }
+`
+
+const Cart = styled(Profile)`
+    margin: 0;
+    background-image: url(${CartImg});
+    @media screen and (max-width: 1024px) {
+    display: flex;
+  }
+
+`
 
 const Wrapper = styled.div`
     display: flex;
     width: 100vw;
     transition: all 0.3s;
-
     display: none;
-
     justify-content: space-between;
     ${props => props.open && css`
         display:flex;
     `}
     position: fixed;
-
     z-index: 2;
     top: 0;
     right: 0px;
@@ -32,6 +179,7 @@ const Fadein = keyframes`
     right: 0px;
   }
 `;
+
 const Fadeout = keyframes`
   0% {
     right: 0px;
@@ -39,15 +187,6 @@ const Fadeout = keyframes`
   100% {
     right: -200px;
   }
-`;
-
-const Appear = keyframes`
-    from {
-        opacity:0;
-    }
-    to {
-        opacity:1;
-    }
 `;
 
 const CartWrapper = styled.div`
@@ -69,8 +208,6 @@ const CartWrapper = styled.div`
     right: 0px;
     color: #868785;
 `;
-
-
 
 const CartOpacity = styled.div`
     width: 100vw;
@@ -120,143 +257,13 @@ const EmptyCart = styled.div`
     margin-top: 40%;
 `;
 
-const Header = () => {
-
-
-    const [open, setOpen] = useState(false);
-    const [close, setClose] = useState(true);
-
-    const onToggle = () => {
-        setOpen(!open)
-        setClose(!close)
-    }
-    console.log(close)
-    
-
-    return(
-        <>
-        <HeaderBlock>
-            <Link to="/" >
-                <Logo/>
-            </Link>
-            <NavWrapper>
-                <NavBar>
-                    <Link to="/shop" style={{ textDecoration: "none", color: "#000" }}>
-                        <Shop>Shop</Shop>
-                    </Link>
-                    <LookBook>
-                        Lookbook
-                    </LookBook>
-                    <StockList>
-                        Stocklist
-                    </StockList>
-                    <Trip>
-                    <a href="http://iacopoapps.appspot.com/hopalongwebgl/" style={{ textDecoration: "none", color: "#000" }}>
-                        Trip
-                        </a>
-                    </Trip>
-                </NavBar>
-                <Link to="/login" style={{ textDecoration: "none", color: "#000" }}>
-                    <Profile/>
-                </Link>
-                <Cart onClick={onToggle} />
-            </NavWrapper>
-        </HeaderBlock>
-        <Wrapper open={open}>
-            <CartWrapper close={close}>
-                <CancleWrapper>
-                    <CancleTitle>Cart</CancleTitle>
-                    <CancleButton onClick={onToggle}/>
-                </CancleWrapper>
-                <EmptyCart/>
-                Cart is empty
-            </CartWrapper>
-            <CartOpacity onClick={onToggle}/>
-        </Wrapper>
-    </>
-    )    
-}
-
-const HeaderBlock = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    position: sticky;
-    top: 8rem;
-    z-index: 1;
-`
-
-const Logo = styled.div`
-    width: 10rem;
-  height: 10rem;
-    border-radius: 5rem;
-  background-image: url(${LogoImg});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  transform: scale(1);
-  transition: all 0.4s ease-in-out;
-  &:hover {
-    transform: scale(1.07);
-  }
-  `
-
-const NavWrapper = styled.div`
-    display: flex;
-    align-items: center;
-`
-
-const NavBar = styled.div`
-    display: flex;
-`
-
-const Shop = styled.div`
-    padding: 0 5rem;
-    &:hover{
-        color: #3D3D3D;
-        cursor: pointer;
-
-    }
-`
-
-const LookBook = styled(Shop)`
-
-`
-
-const StockList = styled(Shop)`
-
-`
-const Trip = styled(Shop)`
-
-`
-
-const Profile = styled.div`
-    width: 5rem;
-    height: 5rem;
-    margin: 0 5rem;
-    background-image: url(${Profileimg});
-background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  transform: scale(1);
-  transition: all 0.4s ease-in-out;
-  &:hover {
-    transform: scale(1.07);
-  }
-`
-
-const Cart = styled(Profile)`
+const MenuIcon = styled(Profile)`
     margin: 0;
-    background-image: url(${CartImg});
-
-`
+    display: none;
+    background-image: url(${MenuImg});
+    @media screen and (max-width: 1024px) {
+    display: flex;
+  }
+`;
 
 export default Header;
